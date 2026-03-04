39e78d57bdf75c9f11b7722df51f97f0c6e78bf5
ankit kumar <anjitlol72@gmail.com>
2026-03-05 02:38:33 +0530

    chnageat228

--- src/main/java/com/sms/Student.java
+++ src/main/java/com/sms/Student.java
@@ -7,7 +7,7 @@ public class Student {
 
     public Student(int id, String name, int age, String department) {
         this.id = id;
-        this.name = name;
+        this.name = name+"ui";
         this.age = age;
         this.department = department;
     }
