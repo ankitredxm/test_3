d503933b0ff9c8ab72ddd2e5733303bbd50a2e2d
ankit kumar <anjitlol72@gmail.com>
2026-03-05 01:34:56 +0530

    changeat134

--- src/main/java/com/sms/Student.java
+++ src/main/java/com/sms/Student.java
@@ -8,7 +8,7 @@ public class Student {
     public Student(int id, String name, int age, String department) {
         this.id = id;
         this.name = name;
-        this.age = age+2;
+        this.age = age+45;
         this.department = department;
     }
 
